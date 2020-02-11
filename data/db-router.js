35417b1844a0211a907a5c9b8f81ae94420d3d20
Kris Monroe <krisdmonroe@gmail.com>
2020-02-11 14:38:20 -0800

    Mvp without testing

---
+++ data/db-router.js
@@ -0,0 +1,126 @@
+const express = require("express");
+
+const Data = require("./db.js");
+
+const router = express.Router();
+
+//
+router.get("/", (req, res) => {
+    Data.find().then(data => {
+        res.status(200).json(data)
+    }).catch(err => {
+        console.log(err)
+        res.status(500).json({error: "The posts information could not be retrieved."})
+    })
+  });
+  //
+  router.get("/:id", (req, res) => {
+    const { id } = req.params;
+
+    Data.findById(id).then(found => {
+        if(found){
+            res.status(200).json(found)
+        } else {
+            res.status(404).json({message: "The post with the specified ID does not exist."})
+        }
+    }).catch(err => {
+        console.log(err)
+        res.status(500).json({error: "The post information could not be retrieved."})
+    })
+  });
+// 
+  router.get("/:id/comments", (req, res) => {
+    const { id } = req.params;
+
+    Data.findCommentById(id).then(found => {
+        if(found){
+            res.status(200).json(found)
+        } else {
+            res.status(500).json({message: "The comment with the specified ID does not exist."})
+        }
+    }).catch(err => {
+        console.log(err)
+        res.status(500).json({error: "The comments information could not be retrieved."})
+  });
+});
+  //
+  router.post("/", (req, res) => {
+    const newPost = req.body
+
+    if(newPost.title && newPost.content){
+        Data.insert(newPost)
+        .then(post => {
+            res.status(201).json(post)
+        }).catch(err => {
+        console.log(err)
+        res.status(500).json({error: "There was an error while saving the post to the database"})
+    })
+  } else {
+    res.status(400).json({ errorMessage: "Please provide title and contents for the post."})
+  }
+  });
+
+// here.................
+  router.post("/:id/comments", (req, res) => {
+    const newPost = req.body
+
+    if(newPost.text){
+        Data.update(req.param.id, newPost)
+        .then(post => {
+            if(post){
+                res.status(200).json(post)
+            } else {
+                res.status(404).json({message: "The post with the specified ID does not exist."})
+            }
+    }).catch(err => {
+        console.log(err)
+        res.status(500).json({ error: "There was an error while saving the comment to the database"})
+    })
+    } else {
+    res.status(400).json({errorMessage: "Please provide text for the comment."})
+    }
+  });
+
+  router.delete("/:id", (req, res) => {
+      const { id } = req.params;
+
+    Data.remove(id).then(found => {
+        if(found){
+            res.status()
+        }else {
+            res.status(404).json({message: "The post with the specified ID does not exist."})
+        }
+    }).catch(err => {
+        console.log(err)
+        res.status(500).json({error: "The post could not be removed"})
+    })
+  });
+
+  // 
+  router.put("/:id", (req, res) => {
+      const changes = req.body;
+
+    if(changes.title && changes.bio){
+        Data.update(req.param.id, changes)
+        .then(post => {
+            if(post){
+                res.status(200).json(post)
+            } else {
+                res.status(404).json({message: "The post with the specified ID does not exist."})
+            }
+    }).catch(err => {
+        console.log(err)
+        res.status(500).json({ error: "The post information could not be modified." })
+    })
+    } else {
+    res.status(400).json({errorMessage: "Please provide title and contents for the post."})
+    }
+  });
+  
+
+ 
+
+
+
+// mind the S in exportS
+module.exports = router; // same as below
---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+
+const server = express();
+
+const apiRouter = require("./data/db-router.js");
+
+server.use(express.json());
+
+server.use("/api/posts", apiRouter);
+
+
+server.get('/', (req, res) => {
+    res.send(`
+      <h2>Lambda Posts API</h>
+      <p>Welcome to the Lambda Posts API</p>
+    `);
+  });
+
+
+
+  const port = 5000;
+  server.listen(port, () => {
+    console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
+  });
+  
